65960de6a1a121065022df93f3c6b17e257b2d08
Artur Bandeira <arturbchanj@usp.br>
2025-02-18 15:02:10 -0300

    oi1

---
+++ submission/fatorial.asm
@@ -0,0 +1,22 @@
+            @ /0000
+            SC FUNC
+            HM /0000
+
+            @ /0200
+FUNC        JP /0000
+            LV /0001
+            MM /0400
+            LD N
+            JZ DEPOIS
+            MM RES
+            MM /0300
+INICIOLOOP  LD /0300
+            SB /0400
+            JZ RETURN
+            MM /0300
+            ML RES
+            MM RES
+            JP INICIOLOOP
+DEPOIS      LV /0001
+            MM RES
+RETURN     RS /0200
